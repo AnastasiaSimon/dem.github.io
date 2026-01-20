1e44443c68ba1ad9103609e38ce8665015dd2f41
Anomalia666 <anastasiaasimonenko09@gmail.com>
2026-01-20 22:57:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const cursor = document.querySelector('.cursor');
 
+// Обновление позиции курсора
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
+
+// Добавление класса при наведении на интерактивные элементы
+const interactiveElements = document.querySelectorAll('a, button, input, textarea, [onclick]');
+
+interactiveElements.forEach(el => {
+  el.addEventListener('mouseenter', () => cursor.classList.add('hover'));
+  el.addEventListener('mouseleave', () => cursor.classList.remove('hover'));
+});
